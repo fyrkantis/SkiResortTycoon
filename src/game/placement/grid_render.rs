c115044610683de7bff08b53ba6b15dab6522f90
David Kniberg <david@kniberg.com>
2025-07-10 16:58:43 +0200

    Made more mesh rendering functions, and they're all terrible.

--- src/game/placement/grid_render.rs
+++ src/game/placement/grid_render.rs
@@ -20,8 +20,9 @@ pub fn setup(
 	grid: Res<Grid>,
 ) {
 	let snow_material = materials.add(Color::WHITE);
+	let mountain_mesh = mountain_mesh_sharp(&grid);
 	commands.spawn((
-		Mesh3d(meshes.add(mountain_mesh(&grid))),
+		Mesh3d(meshes.add(mountain_mesh)),
 		MeshMaterial3d(snow_material),
 	));
 	for (pos, cell) in &grid.cells {
@@ -66,7 +67,93 @@ pub fn setup(
 	}
 }
 
-fn mountain_mesh(grid: &Grid) -> Mesh {
+fn mountain_mesh_sharp(grid: &Grid) -> Mesh {
+	let mut vertices: Vec<Vec3> = Vec::new();
+	for (pos, cell) in grid.cells.iter() {
+		let center_y = cell.height as f32;
+		let [center_x, center_z] = axial_to_xz(pos);
+		let center_cords = Vec3::new(center_x, center_y, center_z);
+		let corner_vertices: Vec<Vec3> = HexCorner::get_array().iter().map(|corner| {
+			let [edge_1, edge_2] = corner.neighbor_edges();
+			// Positions of vertex neighboring cells.
+			let (pos_1, pos_2) = (*pos + edge_1.direction(), *pos + edge_2.direction());
+			let (cell_1_opt, cell_2_opt) = (grid.cells.get(&pos_1), grid.cells.get(&pos_2));
+			let y_avg = match cell_1_opt {
+				Some(cell_1) => match cell_2_opt {
+					Some(cell_2) => (center_y + cell_1.height as f32 + cell_2.height as f32) / 3.,
+					None => (center_y + cell_1.height as f32) / 2.,
+				},
+				None => match cell_2_opt {
+					Some(cell_2) => (center_y + cell_2.height as f32) / 2.,
+					None => center_y,
+				}
+			};
+			let [x, z] = corner.to_xz();
+			Vec3::new(center_x + x, y_avg, center_z - z)
+		}).collect();
+		for i in 0..6 {
+			vertices.push(center_cords);
+			vertices.push(corner_vertices[(i + 1) % 6]);
+			vertices.push(corner_vertices[i]);
+		}
+	}
+	
+	Mesh::new(
+		PrimitiveTopology::TriangleList,
+		RenderAssetUsages::RENDER_WORLD // CPU doesn't touch this mesh after render.
+	)
+	.with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+	.with_inserted_indices(Indices::U32((0..grid.cells.keys().count() as u32 * 18).collect()))
+	.with_computed_smooth_normals()
+}
+
+/// Mesh consisting of soft hexagons.
+fn mountain_mesh_fuzzy(grid: &Grid) -> Mesh {
+	let mut vertices: Vec<Vec3> = Vec::new();
+	let mut vertices_count: u16 = 0;
+	let mut triangles: Vec<u16> = Vec::new();
+	for (pos, cell) in grid.cells.iter() {
+		let center_y = cell.height as f32;
+		let [center_x, center_z] = axial_to_xz(pos);
+		let center_cords = Vec3::new(center_x, center_y, center_z);
+		let center_index = vertices_count;
+		vertices.push(center_cords);
+		HexCorner::get_array().iter().for_each(|corner| {
+			let [edge_1, edge_2] = corner.neighbor_edges();
+			// Positions of vertex neighboring cells.
+			let (pos_1, pos_2) = (*pos + edge_1.direction(), *pos + edge_2.direction());
+			let (cell_1_opt, cell_2_opt) = (grid.cells.get(&pos_1), grid.cells.get(&pos_2));
+			let y_avg = match cell_1_opt {
+				Some(cell_1) => match cell_2_opt {
+					Some(cell_2) => (center_y + cell_1.height as f32 + cell_2.height as f32) / 3.,
+					None => (center_y + cell_1.height as f32) / 2.,
+				},
+				None => match cell_2_opt {
+					Some(cell_2) => (center_y + cell_2.height as f32) / 2.,
+					None => center_y,
+				}
+			};
+			let [x, z] = corner.to_xz();
+			vertices.push(Vec3::new(center_x + x, y_avg, center_z - z));
+		});
+		vertices_count += 7;
+		for i in 0..6 {
+			triangles.push(center_index);
+			triangles.push(center_index + 1 + (1 + i) % 6);
+			triangles.push(center_index + 1 + i);
+		}
+	}
+	
+	Mesh::new(
+		PrimitiveTopology::TriangleList,
+		RenderAssetUsages::RENDER_WORLD // CPU doesn't touch this mesh after render.
+	)
+	.with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+	.with_inserted_indices(Indices::U16(triangles))
+	.with_computed_smooth_normals()
+}
+
+fn mountain_mesh_smooth(grid: &Grid) -> Mesh { // BUG: Some vertices are displaced because later cells interpret the wrong vertices as existing.
 	// Help function that finds the index of the corner vertex with a specific position, or creates one if it doesn't exist.
 	let corner_vertex = |
 		center_pos: Hex,
@@ -86,13 +173,12 @@ fn mountain_mesh(grid: &Grid) -> Mesh {
 					
 				// These and pos are the 3 hex cells surrounding this edge vertex.
 				let (cell_1_pos, cell_2_pos) = (corner_pos, center_pos + edge_2.direction());
-				let y = (center_cords.y
-					+ match grid.cells.get(&cell_1_pos) {Some(cell) => cell.height as f32, None => center_cords.y}
-					+ match grid.cells.get(&cell_2_pos) {Some(cell) => cell.height as f32, None => center_cords.y}
-				) / 3.;
+				let cell_1_y = match grid.cells.get(&cell_1_pos) {Some(cell) => cell.height as f32, None => center_cords.y};
+				let cell_2_y = match grid.cells.get(&cell_2_pos) {Some(cell) => cell.height as f32, None => center_cords.y};
+				let y = (center_cords.y + cell_1_y + cell_2_y) / 3.;
 				
 				let [corner_x, corner_z] = corner.to_xz();
-				let corner_cords = Vec3::new(center_cords.x + corner_x, y, center_cords.z + corner_z);
+				let corner_cords = Vec3::new(center_cords.x + corner_x, y, center_cords.z - corner_z);
 
 				let corner_index = *vertices_count;
 				vertices.push(corner_cords);
@@ -130,6 +216,7 @@ fn mountain_mesh(grid: &Grid) -> Mesh {
 	)
 	.with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
 	.with_inserted_indices(Indices::U16(triangles))
+	.with_computed_smooth_normals()
 }
 
 /// Converts hexx MeshInfo into bevy Mesh.
